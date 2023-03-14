6c5b98a2cd2163c3e98857e73b9337bbf22e6686
Cody Fleenor <35227631+jack-fleenor@users.noreply.github.com>
2023-03-14 14:50:26 -0700

    clean up refactors

--- main.py
+++ main.py
@@ -22,86 +22,57 @@ def processReactorTableData(table):
     reactors = []
     for row in rows:
         reactor_data = pullReactorData(row)
-        print(reactor_data)
         reactors.append(reactor_data)
-    for reactor in reactors:
-        print("processing issued, renewal, and expirary...")
-        if(reactor != None and "issued" in reactor):
-            try:
-                print(f"Issued: {reactor.issued}")
-            except:
-                continue       
-        if(reactor != None and "renewal" in reactor):
-            try:
-                print(f"Renewal: {reactor.renewal}")
-            except:
-                continue
-        if(reactor != None and "expires" in reactor):
-            try:
-                print(f"Expires: {reactor.expires}")
-            except:
-                continue 
-
-def pullReactorData(row):
+    
+def pullReactorData(row):        
     print("Pulling reactor data...")
     reactor_link = row.find('a', href=True)
     if(reactor_link != None):
         reactor_page = requests.get('https://www.nrc.gov' + reactor_link['href'])
         reactor_page = BeautifulSoup(reactor_page.content, "html.parser")
-        reactor_license_issued = reactor_page.find(
-                lambda tag: tag.name == 'strong' and 'Operating License:' in tag.text
-            )
-        reactor_license_renewal = reactor_page.find(
-                lambda tag: tag.name == 'strong' and 'Renewed License:' in tag.text
-            )
-        reactor_license_expires = reactor_page.find(
-                lambda tag: tag.name == 'strong' and 'License Expires:' in tag.text
-            )
+        reactor_license_issued = reactor_page.find(lambda tag: tag.name == 'strong' and 'Operating License:' in tag.text)
+        reactor_license_renewal = reactor_page.find(lambda tag: tag.name == 'strong' and 'Renewed License:' in tag.text)
+        reactor_license_expires = reactor_page.find(lambda tag: tag.name == 'strong' and 'License Expires:' in tag.text)
         reactor_data = { "link": f"https://www.nrc.gov{reactor_link['href']}" }
         
         if(reactor_license_issued != None):
-            temp_str = reactor_license_issued.next_sibling
+            temp = reactor_license_issued.next_sibling
             try:
-                temp = parser.parse(temp_str.split(',')[0], fuzzy=True)
+                temp = parser.parse(temp.split(',')[0], fuzzy=True)
                 ages.append(relativedelta(today, temp).years)
-                temp = temp.strftime('%m/%d/%Y')
+                reactor_data['issued'] = temp.strftime('%m/%d/%Y')
             except:
-                temp = "error"
-            reactor_data['issued'] = temp
+                reactor_data['issued'] = None 
 
         if(reactor_license_renewal != None):
-            temp_str = reactor_license_renewal.next_sibling
+            temp = reactor_license_renewal.next_sibling
             try:
-                temp = parser.parse(temp_str.split(',')[0], fuzzy=True).strftime('%m/%d/%Y')
+                reactor_data['renewal'] = parser.parse(temp.split(',')[0], fuzzy=True).strftime('%m/%d/%Y')
             except:
-                temp = "error"
-            reactor_data['renewal'] = temp
+                reactor_data['renewal'] = None
 
         if(reactor_license_expires != None):
-            temp_str = reactor_license_expires.next_sibling
+            temp = reactor_license_expires.next_sibling
             try:
-                temp = parser.parse(temp_str.split(',')[0], fuzzy=True)
-                temp = temp.strftime('%m/%d/%Y')
-                
+                reactor_data['expires'] = parser.parse(temp.split(',')[0], fuzzy=True).strftime('%m/%d/%Y')
             except:
-                temp = "error"
-            reactor_data['expires'] = temp
+                reactor_data['expires'] = None
 
         return reactor_data
     else:
         return None
 
-        
-if(table != None):
-    processReactorTableData(table)
-    average = sum(ages)/len(ages)
-    print(average)
+def generateChart():
     fig, chart = plt.subplots(1,1)
     chart.hist(ages, len(ages))
     chart.set_title("Nuclear Reactor Ages Histogram")
     chart.set_xlabel("Age (years)")
     chart.set_ylabel("Power Plants")
-    
     plt.show()
+
+
+if(table != None):
+    processReactorTableData(table)
+    generateChart()    
 else: 
     print("error finding table")
